c84267b461f85b33275f55c21889c4a9a76951a4
Grenier Celestin <celestin.grenier@hotmail.com>
2022-05-31 13:02:27 +0100

    backup 13

---
+++ .config/lvim/config.lua
@@ -0,0 +1,22 @@
+vim.keymap.set("n", "<C-c>", ":q<cr>")
+vim.keymap.set("n", "<C-s>", ":w<cr>")
+vim.g['do_filetype_lua'] = 1
+local function with(may, try, nop) return may and try(may) or nop end -- YYY: maybe better off with a proxy approach
+with(lvim, function(lvim)
+  lvim.log.level = "warn"
+  lvim.format_on_save = false
+  lvim.builtin.autopairs.active = false
+  lvim.builtin.nvimtree.show_icons.git = 0
+  lvim.builtin.cmp.completion.keyword_length = 4
+  -- TODO: comment ./.local/share/lunarvim/lvim/lua/lvim/core/nvimtree.lua:163
+end)
+-- added language support
+with(require "nvim-treesitter.parsers", function(parsers)
+  local parser_config = parsers.get_parser_configs()
+  local function setup(name, repo)
+    parser_config[name] = { install_info = { url=repo, branch="main", files={"src/parser.c"}, requires_generate_from_grammar=false } }
+    vim.filetype.add { extension = { [name]=name } }
+  end
+  setup("nmp", "https://github.com/PictElm/tree-sitter-nmp.git")
+  setup("nasm", "https://github.com/PictElm/tree-sitter-nasm.git")
+end)
---
+++ .config/lvim/install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | tee "$0" | bash
