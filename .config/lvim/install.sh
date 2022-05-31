d3cbea9519d19e20ab0af2a65c7d8917c693db09
Grenier Celestin <celestin.grenier@hotmail.com>
2022-05-31 13:42:39 +0100

    backup 15

--- .config/lvim/install.sh
+++ .config/lvim/install.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env sh
-curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | tee "$0" | bash
+#!/usr/bin/env bash
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | tee "$0")
+cp ../lvim.old/config.lua .
+rm -ri ../lvim.old/
