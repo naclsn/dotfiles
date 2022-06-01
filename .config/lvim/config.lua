d7fca820c3f3be6c370bdbd9fdee3bf76913cb95
Grenier Celestin <celestin.grenier@hotmail.com>
2022-06-01 10:20:34 +0100

    backup 18

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,12 +3,16 @@ local function have(a) return a or setmetatable({}, {__index=nop, __call=nop}) e
 local vim, lvim = have(vim), have(lvim)
 vim.keymap.set("n", "<C-c>", ":q<cr>")
 vim.keymap.set("n", "<C-s>", ":w<cr>")
-vim.g['do_filetype_lua'] = 1
+vim.g.do_filetype_lua = 1
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.builtin.autopairs.active = false
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.cmp.completion.keyword_length = 4
+lvim.plugins = {
+  "tpope/vim-surround",
+  keys={"c", "d", "y"},
+}
 -- added language support (REM/TODO: .config/lvim/queries)
 local parser_config = have(nil and require "nvim-treesitter.parsers").get_parser_configs()
 local function ts_grammar(repo, name, branch)
