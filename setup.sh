cf77606bd7863d6e862056bad288583fed5e153d
Grenier Celestin <celestin.grenier@hotmail.com>
2022-08-14 14:10:39 +0200

    (stylus) SoundCloud

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+c=$1
+# -l: list
+# -n: nothing
+# -f: force
+ln() { echo ln "$@"; [ -n = "$c" ] || command ln "$@"; }
 find . -type f -path ./.\* -printf '%h %p\n' -o -name .git -prune | while read dir rel
   do
+    [ -l = "$c" ] && { echo ${rel#./}; continue; }
     [ -d ~/$dir ] || mkdir -p ~/$dir
-    [ -f = "$1" ] && rm -f ~/$rel
-    [ -e ~/$rel ] || ln -s $(realpath $rel) ~/$rel
+    [ -f = "$c" ] && rm -f ~/$rel
+    [ -e ~/$rel ] || ln -s "$(realpath $rel)" ~/$rel
 done
